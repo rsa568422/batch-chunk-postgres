4acce345fa4c00378efa1fc6414dcf335918f3f0
Roberto Sánchez <rsa568422@gmail.com>
2024-10-01 00:10:01 +0200

    Se eliminan clases de dominio no utilizadas en el ejemplo

--- src/main/java/com/example/batch/domain/DatoEntrada.java
+++
@@ -1,9 +0,0 @@
-package com.example.batch.domain;
-
-import lombok.Builder;
-
-import java.math.BigDecimal;
-import java.util.UUID;
-
-@Builder
-public record DatoEntrada(UUID uuid, BigDecimal price, BigDecimal amount) { }
--- src/main/java/com/example/batch/domain/DatoSalida.java
+++
@@ -1,9 +0,0 @@
-package com.example.batch.domain;
-
-import lombok.Builder;
-
-import java.math.BigDecimal;
-import java.util.UUID;
-
-@Builder
-public record DatoSalida(UUID uuid, BigDecimal total) { }
