805254da0e72608cd1a9be25c6e4858989b86d9c
Roberto Sánchez <rsa568422@gmail.com>
2024-10-06 19:30:44 +0200

    Ajustes en fichero de GitHub

--- .github/scripts/pit_score.sh
+++ .github/scripts/pit_score.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
-set -x  # Habilita el modo de depuración
+set -e  # Detiene la ejecución si hay algún error
+
+TOTAL_MUTATIONS=0
+KILLED_MUTATIONS=0
 
 for MODULE in domain infrastructure application; do
-    echo "Procesando módulo: $MODULE"
     PIT_REPORT=$(find ./$MODULE/target/pit-reports -name "index.html" | sort -r | head -n 1)
-    echo "Informe encontrado: $PIT_REPORT"
 
     if [ -f "$PIT_REPORT" ]; then
-        echo "Contenido de la sección Project Summary:"
-        sed -n '/<h3>Project Summary<\/h3>/,/<\/tbody>/p' "$PIT_REPORT"
-
-        echo "Intentando extraer el porcentaje de Mutation Coverage:"
         MUTATION_SCORE=$(sed -n '/<h3>Project Summary<\/h3>/,/<\/tbody>/p' "$PIT_REPORT" |
                          grep -oP '<td>\K\d+(?=% <div class="coverage_bar">)' |
                          sed -n '3p')
 
-        echo "MUTATION_SCORE extraído: $MUTATION_SCORE"
+        MUTATIONS=$(sed -n '/<h3>Project Summary<\/h3>/,/<\/tbody>/p' "$PIT_REPORT" |
+                    grep -oP '<div class="coverage_legend">\K\d+/\d+(?=</div>)' |
+                    sed -n '3p')
 
-        if [ -n "$MUTATION_SCORE" ]; then
-            echo "$MODULE - Porcentaje de mutantes eliminados: $MUTATION_SCORE%"
-        else
-            echo "No se pudo extraer el porcentaje de mutantes eliminados para $MODULE"
-        fi
+        KILLED=$(echo $MUTATIONS | cut -d'/' -f1)
+        TOTAL=$(echo $MUTATIONS | cut -d'/' -f2)
+
+        echo "$MODULE - Porcentaje de mutantes eliminados: $MUTATION_SCORE%"
+
+        TOTAL_MUTATIONS=$((TOTAL_MUTATIONS + TOTAL))
+        KILLED_MUTATIONS=$((KILLED_MUTATIONS + KILLED))
     else
         echo "No se encontró el informe de PIT para $MODULE"
-        ls -R ./$MODULE/target/pit-reports
     fi
 done
+
+if [ $TOTAL_MUTATIONS -gt 0 ]; then
+    TOTAL_PERCENTAGE=$(echo "scale=2; $KILLED_MUTATIONS * 100 / $TOTAL_MUTATIONS" | bc)
+    echo "Porcentaje total de mutantes eliminados: $TOTAL_PERCENTAGE%"
+else
+    echo "No se encontraron mutaciones para calcular el porcentaje total"
+fi
