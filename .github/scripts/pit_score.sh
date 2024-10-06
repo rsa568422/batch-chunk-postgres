dc654cc652cbb1f44d315cf732b678d899bef01a
Roberto Sánchez <rsa568422@gmail.com>
2024-10-06 18:15:49 +0200

    Ajustes en fichero de GitHub

--- .github/scripts/pit_score.sh
+++ .github/scripts/pit_score.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+set -x  # Habilita el modo de depuración
+
 for MODULE in domain infrastructure application; do
+    echo "Procesando módulo: $MODULE"
     PIT_REPORT=$(find ./$MODULE/target/pit-reports -name "index.html" | sort -r | head -n 1)
+    echo "Informe encontrado: $PIT_REPORT"
+
     if [ -f "$PIT_REPORT" ]; then
+        echo "Contenido del informe:"
+        cat "$PIT_REPORT"
+
         MUTATION_SCORE=$(grep -oP 'Mutation Coverage.*?<td>(\d+(?:\.\d+)?)%' "$PIT_REPORT" | grep -oP '\d+(?:\.\d+)?')
+        echo "MUTATION_SCORE extraído: $MUTATION_SCORE"
+
         if [ -n "$MUTATION_SCORE" ]; then
             echo "$MODULE - Porcentaje de mutantes eliminados: $MUTATION_SCORE%"
         else
