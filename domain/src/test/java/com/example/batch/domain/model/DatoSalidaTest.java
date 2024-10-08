d0c12dbdeb7f8199977373282170d692eda6b57b
Roberto Sánchez <rsa568422@gmail.com>
2024-10-08 17:02:35 +0200

    Mejoras en testing para eliminar mutantes

--- domain/src/test/java/com/example/batch/domain/model/DatoSalidaTest.java
+++ domain/src/test/java/com/example/batch/domain/model/DatoSalidaTest.java
@@ -7,28 +7,39 @@ import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DatoSalidaTest {
 
     @Test
     void lombok() {
-        var actual = DatoSalida.builder().build();
-        actual.setId(Data.SALIDA.getId());
-        actual.setTotal(Data.SALIDA.getTotal());
+        // given
+        var uuid = "cda33687-2f86-4da5-828c-aa7f95a098e3";
+        var total = BigDecimal.valueOf(360);
+        var hashCode = 839205166;
+        var toString = "DatoSalida(id=cda33687-2f86-4da5-828c-aa7f95a098e3, total=360)";
 
+        // when
+        var actual = DatoSalida.builder().build();
+        actual.setId(uuid);
+        actual.setTotal(total);
+
+        // then
         assertAll(
                 () -> assertNotNull(actual),
+                () -> assertEquals(uuid, actual.getId()),
+                () -> assertEquals(total, actual.getTotal()),
                 () -> assertEquals(Data.SALIDA, actual),
-                () -> assertEquals(Data.SALIDA.getId(), actual.getId()),
-                () -> assertEquals(Data.SALIDA.getTotal(), actual.getTotal()),
-                () -> assertEquals(Data.SALIDA.hashCode(), actual.hashCode()),
-                () -> assertEquals(Data.SALIDA.toString(), actual.toString()),
+                () -> assertTrue(actual.equals(actual)),
+                () -> assertFalse(actual.equals(null)),
+                () -> assertFalse(actual.equals(BigDecimal.TEN)),
+                () -> assertEquals(hashCode, actual.hashCode()),
+                () -> assertEquals(toString, actual.toString()),
                 () -> {
-                    var newActual = DatoSalida.builder().build();
-                    actual.setId(Data.SALIDA.getId());
-                    actual.setTotal(BigDecimal.TEN);
+                    var newActual = DatoSalida.builder().id(uuid).total(BigDecimal.TEN).build();
                     assertNotEquals(Data.SALIDA, newActual);
                 }
         );
