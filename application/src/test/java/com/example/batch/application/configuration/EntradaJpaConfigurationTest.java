3e4895bdb75c306a2328c8af2f8270937020516a
Roberto Sánchez <rsa568422@gmail.com>
2024-10-13 01:25:18 +0200

    Mejoras en testing

---
+++ application/src/test/java/com/example/batch/application/configuration/EntradaJpaConfigurationTest.java
@@ -0,0 +1,73 @@
+package com.example.batch.application.configuration;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+import static com.example.batch.application.configuration.Constants.ENTRADA;
+import static com.example.batch.application.configuration.Constants.ENTRADA_ENTITY_PACKAGE;
+import static com.example.batch.application.configuration.Constants.HIBERNATE_DIALECT;
+import static com.example.batch.application.configuration.Constants.HIBERNATE_HBM_2_DDL_AUTO;
+import static com.example.batch.application.configuration.Constants.POSTGRES_SQL_DIALECT;
+import static com.example.batch.application.configuration.Constants.UPDATE;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+class EntradaJpaConfigurationTest {
+
+    private final EntradaJpaConfiguration entradaJpaConfiguration = new EntradaJpaConfiguration();
+
+    @Test
+    void entradaEntityManagerFactory() {
+        // given
+        var entityManagerFactoryBuilder = Mockito.mock(EntityManagerFactoryBuilder.class);
+        var builder = Mockito.mock(EntityManagerFactoryBuilder.Builder.class);
+        var dataSource = Mockito.mock(DataSource.class);
+        var properties = Map.of(HIBERNATE_HBM_2_DDL_AUTO, UPDATE, HIBERNATE_DIALECT, POSTGRES_SQL_DIALECT);
+        var expected = Mockito.mock(LocalContainerEntityManagerFactoryBean.class);
+
+        when(entityManagerFactoryBuilder.dataSource(dataSource)).thenReturn(builder);
+        when(builder.packages(ENTRADA_ENTITY_PACKAGE)).thenReturn(builder);
+        when(builder.persistenceUnit(ENTRADA)).thenReturn(builder);
+        when(builder.properties(properties)).thenReturn(builder);
+        when(builder.build()).thenReturn(expected);
+
+        // when
+        var actual = entradaJpaConfiguration.entradaEntityManagerFactory(entityManagerFactoryBuilder, dataSource);
+
+        // then
+        assertAll(
+                () -> assertNotNull(actual),
+                () -> assertSame(expected, actual)
+        );
+
+        verify(entityManagerFactoryBuilder, times(1)).dataSource(dataSource);
+        verify(builder, times(1)).packages(ENTRADA_ENTITY_PACKAGE);
+        verify(builder, times(1)).persistenceUnit(ENTRADA);
+        verify(builder, times(1)).properties(properties);
+        verify(builder, times(1)).build();
+        verifyNoMoreInteractions(entityManagerFactoryBuilder, builder);
+    }
+
+    @Test
+    void entradaTransactionManager() {
+        // given
+        var entityManagerFactory = Mockito.mock(EntityManagerFactory.class);
+
+        // when
+        var actual = entradaJpaConfiguration.entradaTransactionManager(entityManagerFactory);
+
+        // then
+        assertNotNull(actual);
+    }
+}
